403fb3cdec7402856e663d98a44aa0e7d8d80ac2
SnTest <3197661591@qq.com>
2026-09-25 16:10:10 +0800

    流式输出

--- src/main/java/cn/itcast/demo/springaistudy/controller/ChatController.java
+++ src/main/java/cn/itcast/demo/springaistudy/controller/ChatController.java
@@ -4,6 +4,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
@@ -22,4 +23,14 @@ public class ChatController {
                 .call()//同步调用
                 .content();//取文本结果
     }
+
+    @GetMapping(value = "/stream",produces = "text/html;charset=UTF-8")
+    public Flux<String> stream(@RequestParam String message){
+        return  chatClient.prompt()
+                .user(message)
+                .stream()//流式调用
+                .content();
+
+    }
+
 }
