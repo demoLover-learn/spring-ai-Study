135c092bcc70c14ed79bb322bb79589f6024152f
SnTest <3197661591@qq.com>
2026-09-25 14:56:22 +0800

    first commit

---
+++ src/main/java/cn/itcast/demo/springaistudy/SpringAiStudyApplication.java
@@ -0,0 +1,13 @@
+package cn.itcast.demo.springaistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/itcast/demo/springaistudy/SpringAiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itcast.demo.springaistudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
