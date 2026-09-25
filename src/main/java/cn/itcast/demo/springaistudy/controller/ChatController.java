bb6fa9578add712d91d7ec6c8792a8ac486357ab
SnTest <3197661591@qq.com>
2026-09-25 15:08:41 +0800

    同步调用

---
+++ src/main/java/cn/itcast/demo/springaistudy/controller/ChatController.java
@@ -0,0 +1,25 @@
+package cn.itcast.demo.springaistudy.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    //chatclient.builder由spring ai自动配置注入
+    public ChatController(ChatClient.Builder builder){
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String message){
+        return chatClient.prompt()//创建第一次对话
+                .user(message)//用户输入
+                .call()//同步调用
+                .content();//取文本结果
+    }
+}
